5830a36648e23691580892aaeb61963851634209
TejasriJuvvireddy <127474223+TejasriJuvvireddy@users.noreply.github.com>
2023-08-18 12:04:22 +0530

    import java.util.Scanner;        public class Nmultiples{          public static void main(String[]args) {             Scanner scanner=new Scanner(System.in);             int n=scanner.nextInt();             int i;             for(i = 1;i <= n; i++)
                 System.out.printf("%d ",3*i);
    
    
           }
    }

---
+++ First_N_multiples_of_3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+       public class Nmultiples{
+         public static void main(String[]args) {
+            Scanner scanner=new Scanner(System.in);
+            int n=scanner.nextInt();
+            int i;
+            for(i = 1;i <= n; i++)
+         
+
+             System.out.printf("%d ",3*i);
+
+
+       }
+}
+
+
+
